4b81803b071b1d46200a75b7d4d74f200ba86ed1
qingwuit <bishashiwo@gmail.com>
2021-01-04 18:33:00 +0800

    20200104 eidt.

--- database/migrations/2020_07_15_155322_create_advs_table.php
+++ database/migrations/2020_07_15_155322_create_advs_table.php
@@ -20,8 +20,8 @@ class CreateAdvsTable extends Migration
             $table->string('name',20)->default('')->comment('广告名称');
             $table->string('url',100)->default('#')->comment('链接');
             $table->string('image_url',150)->default('')->comment('图片地址');
-            $table->timestamp('adv_start')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('开始时间');
-            $table->timestamp('adv_end')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('结束时间');
+            $table->timestamp('adv_start')->comment('开始时间');
+            $table->timestamp('adv_end')->comment('结束时间');
             $table->unsignedInteger('adv_sort')->default(0)->comment('排序');
             $table->unsignedInteger('adv_type')->default(0)->comment('类型');
             $table->timestamps();
