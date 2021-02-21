d6a5229a90df97aeb36a20097cae2de6bf7a18eb
qingwuit <bishashiwo@gmail.com>
2021-02-21 16:29:38 +0800

    20210221 edit .

--- app/Http/Resources/Home/CartResource/CartCollection.php
+++ app/Http/Resources/Home/CartResource/CartCollection.php
@@ -21,25 +21,25 @@ class CartCollection extends ResourceCollection
                 return [
                     'store_id'              =>  $item->store->id,
                     'user_id'               =>  $item->user_id,
-                    'store_name'            =>  $item->store->store_name,
-                    'store_logo'            =>  $item->store->store_logo,
+                    'store_name'            =>  $item->store->store_name??'',
+                    'store_logo'            =>  $item->store->store_logo??'',
                     'checked'               =>  false,
                     'css'                   =>  false,
                     'cart_list'             =>  $item->carts->map(function($cartItem){
 
                                                     // 是否存在sku 
-                                                    $goods_image = $this->thumb($cartItem->goods->goods_master_image,150);
-                                                    $goods_price = $cartItem->goods->goods_price;
+                                                    $goods_image = empty($cartItem->goods)?'':$this->thumb($cartItem->goods->goods_master_image,150);
+                                                    $goods_price = $cartItem->goods->goods_price??'0.00';
                                                     $sku_name = '-';
                                                     if(!empty($cartItem->goods_sku)){
-                                                        $goods_price = $cartItem->goods_sku->goods_price;
-                                                        $sku_name = $cartItem->goods_sku->sku_name;
+                                                        $goods_price = $cartItem->goods_sku->goods_price??'0.00';
+                                                        $sku_name = $cartItem->goods_sku->sku_name??'-';
                                                     }
                                                     return [
                                                         'cart_id'               =>  $cartItem->id,
                                                         'goods_id'              =>  $cartItem->goods_id,
                                                         'sku_id'                =>  $cartItem->sku_id,
-                                                        'goods_name'            =>  $cartItem->goods->goods_name,
+                                                        'goods_name'            =>  $cartItem->goods->goods_name??__('goods.goods_not_found'),
                                                         'buy_num'               =>  $cartItem->buy_num,
                                                         'goods_image'           =>  $goods_image,
                                                         'goods_price'           =>  $goods_price,
