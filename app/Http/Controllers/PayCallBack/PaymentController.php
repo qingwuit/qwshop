d0329aeeaedea4d8a79a228edd629a3e2c0d8a6a
qingwuit <bishashiwo@gmail.com>
2021-03-14 20:09:13 +0800

    20210314 PaymentCall.

--- app/Http/Controllers/PayCallBack/PaymentController.php
+++ app/Http/Controllers/PayCallBack/PaymentController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers\PayCallBack;
 use App\Http\Controllers\Controller;
 use App\Services\PayMentService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PaymentController extends Controller
 {
     // 回调处理
     public function payment($payment_name){
         $payment_service = new PayMentService();
-        return $payment_service->payment($payment_name);
+        $rs = $payment_service->payment($payment_name);
+        if($rs['status']){
+            return $rs['data'];
+        }else{
+            Log::channel('qwlog')->debug($payment_name.'-'.$rs['msg']);
+        }
     }
 }
