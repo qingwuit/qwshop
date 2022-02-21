13c428365ef9ce00614cdf9dc27391ac3b05aec3
qingwuit <bishashiwo@gmail.com>
2022-02-21 17:17:35 +0800

    online chat add .

--- app/Http/Controllers/Chat/IndexController.php
+++ app/Http/Controllers/Chat/IndexController.php
@@ -21,10 +21,10 @@ class IndexController extends Controller
             } catch (\Exception $e) {
                 $id = md5($ip);
             }
-        }else{
+        } else {
             $id = md5($ip);
         }
-        
+
         $friends = $this->getService('ChatFriend', true)->orWhere(function ($q) use ($provider, $id) {
             return $q->where('sid', $id)->where('stype', $provider);
         })->orWhere(function ($q) use ($provider, $id) {
@@ -48,10 +48,10 @@ class IndexController extends Controller
             } catch (\Exception $e) {
                 $id = md5($ip);
             }
-        }else{
+        } else {
             $id = md5($ip);
         }
-        
+
         $data = $this->getService('ChatContent', true)->orWhere(function ($q) use ($provider, $id, $rid, $rtype) {
             return $q->where('sid', $id)->where('stype', $provider)->where('rid', $rid)->where('rtype', $rtype);
         })->orWhere(function ($q) use ($provider, $id, $rid, $rtype) {
@@ -75,10 +75,10 @@ class IndexController extends Controller
             } catch (\Exception $e) {
                 $id = md5($ip);
             }
-        }else{
+        } else {
             $id = md5($ip);
         }
-        
+
         $model = $this->getService('ChatContent', true);
         $model->sid = $id;
         $model->stype = $provider;
@@ -107,7 +107,7 @@ class IndexController extends Controller
                 'rid'           =>  $model->rid,
                 'rtype'         =>  $model->rtype,
             ]
-        ])."\n");
+        ]) . "\n");
         // echo fread($client, 8192);
         stream_set_timeout($client, 2);  //这个timeout 和 stream_socket_client()的参数timeout 区别是什么？？
         stream_socket_shutdown($client, STREAM_SHUT_RDWR);  //这样就算是关闭了连接，释放内存资源了
@@ -129,16 +129,16 @@ class IndexController extends Controller
             } catch (\Exception $e) {
                 $id = md5($ip);
             }
-        }else{
+        } else {
             $id = md5($ip);
         }
-        
 
-        if($id == $rid && $provider == $rtype) return $this->error("You can't talk to yourself.");
+
+        if ($id == $rid && $provider == $rtype) return $this->error("You can't talk to yourself.");
 
         // 判断是否有存在rid
-        if(empty($rid) && empty($rtype)){
-            return $this->success(['sid'=>$id,'stype'=>$provider]);
+        if (empty($rid) && empty($rtype)) {
+            return $this->success(['sid' => $id, 'stype' => $provider]);
         }
 
         // 修改好友最后一次聊天时间
@@ -148,7 +148,7 @@ class IndexController extends Controller
             return $q->where('rid', $id)->where('rtype', $provider)->where('sid', $rid)->where('stype', $rtype);
         })->first();
 
-        if(!$friend){
+        if (!$friend) {
             $friend = $this->getService('ChatFriend', true);
             $friend->sid = $id;
             $friend->stype = $provider;
@@ -156,10 +156,10 @@ class IndexController extends Controller
             $friend->rtype = $rtype;
             $friend->updated_at = now();
             $friend->save();
-        }else{
+        } else {
             $friend->updated_at = now();
             $friend->save();
         }
-        return $this->success(['sid'=>$id,'stype'=>$provider]);
+        return $this->success(['sid' => $id, 'stype' => $provider]);
     }
 }
