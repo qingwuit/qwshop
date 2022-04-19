918923299fd9dfa5fe16572c8b4bf1b523eb0e26
qingwuit <bishashiwo@gmail.com>
2022-04-19 22:01:54 +0800

    Dockerfile add .

--- dockerfiles/install.sh
+++ dockerfiles/install.sh
@@ -1,5 +1,7 @@
-#! /bin/bash
-cp ../env.example ../env
-rm -rf ../node_modules && rm -rf ../vendor
+!/bin/bash
+
+cp /var/www/.env.example /var/www/.env
+rm -rf /var/www/node_modules && rm -rf /var/www/vendor
 composer install
 npm install && npm run prod
+php artisan docker:install
